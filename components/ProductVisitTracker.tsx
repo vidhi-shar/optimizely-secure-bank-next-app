91eed5d9b86662fbb687c124ceee0a0380d8cf04
Vidhi Sharma <vidhi_s@hcltech.com>
2026-06-24 16:55:15 +0530

    REsolved ProductVisitTracker issue

--- components/ProductVisitTracker.tsx
+++ components/ProductVisitTracker.tsx
@@ -6,10 +6,19 @@ export default function ProductVisitTracker({
   product,
 }: {
   product: "credit-cards" | "loans";
-}) {
+}) {  
   useEffect(() => {
-    document.cookie = `sb_visited_product=${product}; path=/; max-age=2592000`;
-  }, [product]);
+    if (typeof window !== 'undefined') {
+      const path = window.location.pathname;
+      if (path === '/credit-cards') {
+        document.cookie =
+          "sb_visited_product=credit-cards; path=/; max-age=2592000";
+      } else if (path === '/loans') {
+        document.cookie =
+          "sb_visited_product=loans; path=/; max-age=2592000";
+      }
+    }
+  }, []);
 
   return null;
 }
